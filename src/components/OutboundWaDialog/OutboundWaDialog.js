f817d9685e9316ef173680308941e6f64d4dc3b6
Gabriel Oliveira <gabriel.oliveira@evoluservices.com>
2022-06-03 19:14:34 -0300

    Inclui mensagem inicial na modal
    Signed-off-by: Gabriel Oliveira <gabriel.oliveira@evoluservices.com>

--- src/components/OutboundWaDialog/OutboundWaDialog.js
+++ src/components/OutboundWaDialog/OutboundWaDialog.js
@@ -1,11 +1,21 @@
 import React from "react";
-import Dialog from "@material-ui/core/Dialog";
-import DialogContent from "@material-ui/core/DialogContent";
-import Button from '@material-ui/core/Button'
 import { withTheme, Actions } from "@twilio/flex-ui";
-import * as Flex from "@twilio/flex-ui";
-import { Container , Title , Label, ContactData} from "./OutboundWaDialog.styles";
 import taskService from "../../services/TaskService";
+import {
+  Combobox,
+  Modal,
+  Button,
+  ModalHeader,
+  ModalHeading,
+  ModalBody,
+  ModalFooter,
+  ModalFooterActions,
+  Input,
+  Label,
+  Paragraph,
+  Stack
+} from '@twilio-paste/core';
+import Handlebars from "handlebars";
 
 
 class OutboundWaDialog extends React.Component {
@@ -17,7 +27,9 @@ class OutboundWaDialog extends React.Component {
     this.createTask = this.createTask.bind(this);
     this.state = {
       open: false,
-      phoneNumber: ''
+      phoneNumber: '',
+      selectedTemplate: '',
+      templateInputs: {}
     };
   }
 
@@ -42,6 +54,7 @@ class OutboundWaDialog extends React.Component {
   }
 
   setPhone(phoneNumber) {
+    phoneNumber = '+55' + phoneNumber;
     this.setState({phoneNumber});
   }
 
@@ -51,34 +64,100 @@ class OutboundWaDialog extends React.Component {
     this.setState({open: false});
   }
 
+  interpolateMessage() {
+    const {selectedTemplate, templateInputs} = this.state;
+    const template = Handlebars.compile(selectedTemplate);
+    return template(templateInputs);
+  }
+
+  createTemplateInputs() {
+    return [...this.state.selectedTemplate.matchAll(/\{\{([0-9]+)\}\}/g)].map(match => {
+      const placeholder = match[0];
+      const inputName = match[1];
+      const value = this.state.templateInputs[inputName];
+
+      return <Input
+        placeholder={placeholder}
+        value={value === placeholder ? '' : value}
+        onChange={e => {
+          this.setState({
+            ...this.state,
+            templateInputs: {
+              ...this.state.templateInputs,
+              [inputName]: e.target.value || placeholder
+            }
+          });
+        }}
+        required
+      />
+    });
+  }
+
   render() {
+    const templates = [
+      "Olá {{1}} 😃! Sou {{2}}, agente de relacionamento da sua conta e acompanho você aqui na {{3}} 🤩.\nPodemos conversar por aqui?",
+      'Adrienne Maree Brown',
+      'Octavia Butler'];
+
     return (
-      <Dialog
-        open={this.state.open}
-        onClose={this.cancelForm}
-        aria-labelledby="form-dialog-title"
-        maxWidth={"md"}
-        fullWidth={true}
-      >
-        <DialogContent>
-            <Container>
-                <Title>Enviar mensagem por Whatsapp</Title>
-                <ContactData>
-                    <Label>Número</Label>
-                    <input type="text" id="phoneNumber"
-                        placeholder="DDD + Número"
-                        onChange={e => this.setPhone(e.target.value)}
-                        onBlur={(e) => {
-                            this.setPhone('+55' + e.target.value);
-                          }}    
-                    />
-                </ContactData>
-                <div>
-                    <Button variant="contained" color="secondary" onClick={this.createTask} >Enviar</Button>
-                </div>
-            </Container>
-        </DialogContent>
-      </Dialog>
+      <Modal isOpen={this.state.open} onDismiss={this.cancelForm} size="wide">
+        <ModalHeader>
+          <ModalHeading as="h3">
+            Iniciar uma conversa por Whatsapp
+          </ModalHeading>
+        </ModalHeader>
+        <ModalBody style={{minHeight: 400}} element="MODAL_BODY_HEIGHT_400">
+          <Label htmlFor="phoneNumber" required>Número</Label>
+
+          <Input
+            id="phoneNumber"
+            name="phoneNumber"
+            type="text"
+            placeholder="DDD + Número"
+            onChange={e => this.setPhone(e.target.value)}
+            required
+          />
+
+          <Combobox
+            items={templates}
+            labelText="Selecione um template"
+            selectedItem={this.state.selectedTemplate}
+            onSelectedItemChange={comboState => {
+              const templateInputs = {};
+              const selectedTemplate = comboState.selectedItem;
+
+              [...selectedTemplate.matchAll(/\{\{([0-9]+)\}\}/g)].forEach(([value, name]) => {
+                templateInputs[name] = value;
+              });
+
+              this.setState({
+                ...this.state,
+                selectedTemplate,
+                templateInputs
+              });
+            }}
+            required />
+
+          <Stack orientation="horizontal" spacing="space60">
+            {this.createTemplateInputs()}
+          </Stack>
+
+          <Paragraph>
+            {this.interpolateMessage()}
+          </Paragraph>
+
+        </ModalBody>
+        <ModalFooter>
+          <ModalFooterActions>
+            <Button variant="secondary" onClick={this.cancelForm}>
+              Cancelar
+            </Button>
+            <Button variant="primary" onClick={this.createTask}>
+              Enviar
+            </Button>
+          </ModalFooterActions>
+        </ModalFooter>
+      </Modal>
     );
   }
 }
