a055af4e99d0941921dae92ec318dfefb5458284
Daniel Alves <daniel.alves@evoluservices.com>
2022-11-17 16:46:33 -0300

    Implements task interactions into outbound twilio plugin

--- src/OutboundWhatsappPlugin.js
+++ src/OutboundWhatsappPlugin.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { VERSION } from '@twilio/flex-ui';
-import { FlexPlugin } from '@twilio/flex-plugin';
-import { CustomizationProvider } from "@twilio-paste/core/customization";
-import NewWaTaskButton  from './components/NewWaTaskButton';
-import OutboundWaDialog from './components/OutboundWaDialog/OutboundWaDialog';
-import CannedResponses from './components/CannedResponses/CannedResponses';
+import React from "react"
+import { VERSION } from "@twilio/flex-ui"
+import { FlexPlugin } from "@twilio/flex-plugin"
+import { CustomizationProvider } from "@twilio-paste/core/customization"
+import NewWaTaskButton from "./components/NewWaTaskButton"
+import OutboundWaDialog from "./components/OutboundWaDialog/OutboundWaDialog"
+import CannedResponses from "./components/CannedResponses/CannedResponses"
 
+import reducers, { namespace } from "./states"
 
-import reducers, { namespace } from './states';
-
-const PLUGIN_NAME = 'OutboundWhatsappPlugin';
+const PLUGIN_NAME = "OutboundWhatsappPlugin"
 
 export default class OutboundWhatsappPlugin extends FlexPlugin {
   constructor() {
-    super(PLUGIN_NAME);
+    super(PLUGIN_NAME)
   }
 
   /**
@@ -24,50 +23,33 @@ export default class OutboundWhatsappPlugin extends FlexPlugin {
    * @param manager { import('@twilio/flex-ui').Manager }
    */
   init(flex, manager) {
-    this.registerReducers(manager);
+    this.registerReducers(manager)
 
     flex.setProviders({
-      PasteThemeProvider: CustomizationProvider
-    });
+      PasteThemeProvider: CustomizationProvider,
+    })
 
     /*Outbound WhatsApp button*/
-    flex.MainHeader.Content.add(<NewWaTaskButton key="outbound-whatsapp-button"/>, {sortOrder: -999, align: 'end'})
+    flex.MainHeader.Content.add(
+      <NewWaTaskButton key="outbound-whatsapp-button" />,
+      { sortOrder: -999, align: "end" },
+    )
 
     /*Custom action to dispatch an event and open the modal screen*/
     flex.Actions.registerAction("waModalControl", (payload) => {
-      var event = new Event("waModalControlOpen");
-      document.dispatchEvent(event);
-      return Promise.resolve();
-    });
+      var event = new Event("waModalControlOpen")
+      document.dispatchEvent(event)
+      return Promise.resolve()
+    })
 
     /*Outbound Whatsapp Dialog */
     flex.MainContainer.Content.add(<OutboundWaDialog key="imageModal" />, {
-      sortOrder: 1
-    });
+      sortOrder: 1,
+    })
 
     /* Agent auto-responses */
 
-    flex.MessageInputV2.Content.add(<CannedResponses key="canned-responses" />);
-  
-    
-    /* Register event listener for new reservations so Flex will auto-accept outbound tasks */
-    manager.workerClient.on('reservation.created', reservation => {
-      const { sid , task } = reservation;
-      console.log(task.attributes);
-
-      if(task.attributes.channelType === 'whatsapp' && task.attributes.direction === 'outbound'){
-        console.log('New outbound task received');
-        
-        flex.Actions.invokeAction('AcceptTask', {
-          sid
-        });
-
-        flex.Actions.invokeAction('SelectTask', {
-          sid
-        });
-      }
-    });
-
+    flex.MessageInputV2.Content.add(<CannedResponses key="canned-responses" />)
   }
 
   /**
@@ -78,10 +60,12 @@ export default class OutboundWhatsappPlugin extends FlexPlugin {
   registerReducers(manager) {
     if (!manager.store.addReducer) {
       // eslint: disable-next-line
-      console.error(`You need FlexUI > 1.9.0 to use built-in redux; you are currently on ${VERSION}`);
-      return;
+      console.error(
+        `You need FlexUI > 1.9.0 to use built-in redux; you are currently on ${VERSION}`,
+      )
+      return
     }
 
-    manager.store.addReducer(namespace, reducers);
+    manager.store.addReducer(namespace, reducers)
   }
 }
